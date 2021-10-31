d99e3e6c519737357df07709db76c818cfec2d5f
Demidao <Proletarka1>
2021-10-31 23:09:44 +0300

    Initial commit

---
+++ src/main/java/com/demidao/SpringBootApiAppApplication.java
@@ -0,0 +1,13 @@
+package com.demidao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApiAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demidao/springbootapiapp/SpringBootApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demidao.springbootapiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
